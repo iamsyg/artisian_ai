7f2000b2d0b7c344d2cab8c779be61241f38ee5f
Swayam <swayamg741@gmail.com>
2025-09-26 20:29:15 +0530

    profile: sidebar UI

--- frontend/app/(app)/profile/page.tsx
+++ frontend/app/(app)/profile/page.tsx
@@ -1,24 +1,11 @@
-"use client";
-
-import { supabase } from '@/app/lib/supabaseClient';
+import Sidebar from '@/app/components/Sidebar'
 import React from 'react'
 
 const page = () => {
-
-  const handleSignOut = async() => {
-    // Sign out logic here
-    const {error} = await supabase.auth.signOut();
-    if (error) console.log('Error signing out:', error.message);
-    else window.location.href = '/store';
-  }
-
   return (
     <div>
-      <button onClick={handleSignOut}>
-        sign out
-      </button>
+      <Sidebar />
     </div>
   )
 }
-
-export default page 
+export default page
